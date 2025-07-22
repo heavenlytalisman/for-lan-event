2a1b5d1d55dd541a52e488496b0c3ac84d301021
heavenlytalisman <173621005+heavenlytalisman@users.noreply.github.com>
2025-07-22 17:10:41 +0530

    feat(feature/requirements-install): add auto-install game requirements

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
 import gdown
 import os
 import zipfile
+import requests
+import subprocess
+
+
+req1_url = "https://aka.ms/vs/17/release/vc_redist.x86.exe"
+req2_url = "https://aka.ms/vs/17/release/vc_redist.x64.exe"
+
+req1_name = "vcredist.x86.exe"
+req2_name = "vcredist.x64.exe"
+
+print("Downloading and installing requirements...")
+response1 = requests.get(req1_url, stream=True)
+response2 = requests.get(req2_url, stream=True)
+
+with open(req1_name, "wb") as f1:
+    for chunk in response1.iter_content(chunk_size=65536):
+        if chunk:
+            f1.write(chunk)
+
+subprocess.run([req1_name], shell=True)
+
+
+with open(req2_name, "wb") as f2:
+    for chunk in response2.iter_content(chunk_size=65536):
+        if chunk:
+            f2.write(chunk)
+
+subprocess.run([req2_name], shell=True)
 
 
 gdrive_link = "https://drive.google.com/file/d/1Fox-l7wnw_0CwPaQlphPm2J7tieSdELt/view?usp=drive_link"
 
 filename = "MP.zip"
 ex_folder = "MP"
+
+print("Downloading the game...")
 
 gdown.download(gdrive_link, filename, quiet=False)
 
@@ -17,5 +47,5 @@ with zipfile.ZipFile(filename, mode ="r") as zref:
     zref.extractall(ex_folder)
 
 os.remove(filename)
-
+print("Done.")
 
