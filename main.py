1fa1499dc284c46926659572897b051001c83295
heavenlytalisman <173621005+heavenlytalisman@users.noreply.github.com>
2025-07-22 22:17:07 +0530

    feat(shortcut): add shortcut creation feature

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import zipfile
 import requests
 import subprocess
+from win32com.client import Dispatch
 
 
 req1_url = "https://aka.ms/vs/17/release/vc_redist.x86.exe"
@@ -20,8 +21,8 @@ with open(req1_name, "wb") as f1:
         if chunk:
             f1.write(chunk)
 
-if subprocess.run([req1_name], shell=True):
-    os.remove(req1_name)
+subprocess.run([req1_name], shell=True)
+os.remove(req1_name)
 
 
 with open(req2_name, "wb") as f2:
@@ -29,8 +30,8 @@ with open(req2_name, "wb") as f2:
         if chunk:
             f2.write(chunk)
 
-if subprocess.run([req2_name], shell=True):
-    os.remove(req2_name)
+subprocess.run([req2_name], shell=True)
+os.remove(req2_name)
 
 
 file_id = "1WiymAn2C4tGWxu5qBCHeLpvsshsZEvDe"
@@ -53,3 +54,24 @@ if (gdown.download(gdrive_link, filename, quiet=False, use_cookies=True)):
 
 else:
     print("Error")
+
+
+exe_path = os.path.abspath(os.path.join(ex_folder, "GameData", "taystjk.x86_64.exe"))
+shortcut_name = "JKMP"
+desktop = os.path.join(os.path.expanduser("~"), "Desktop")
+shortcut_path1 = os.path.join(desktop, f"{shortcut_name}.lnk")
+shortcut_path2 = os.path.join(ex_folder, f"{shortcut_name}.lnk")
+
+shell = Dispatch("WScript.Shell")
+
+lnk1 = shell.CreateShortcut(shortcut_path1)
+lnk1.TargetPath = exe_path
+lnk1.WorkingDirectory = os.path.dirname(exe_path)
+lnk1.Save()
+
+lnk2 = shell.CreateShortcut(shortcut_path2)
+lnk2.TargetPath = exe_path
+lnk2.WorkingDirectory = os.path.dirname(exe_path)
+lnk2.Save()
+
+print("Shortcuts created successfully!")
